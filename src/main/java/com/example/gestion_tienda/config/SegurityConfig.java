2f520c92e820631779487e9daf7c21bce443f241
angeldavila00 <angeldavila0975@gmail.com>
2026-03-09 08:42:03 -0500

    feat: :sparkles: fix introduccion the values

--- src/main/java/com/example/gestion_tienda/config/SegurityConfig.java
+++ src/main/java/com/example/gestion_tienda/config/SegurityConfig.java
@@ -46,7 +46,7 @@ public class SegurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:63343"));
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:63343", "http://localhost:63342"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
