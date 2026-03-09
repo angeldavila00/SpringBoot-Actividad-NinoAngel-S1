714fbb71aeeaf1ad511b9d9a366c76d9d5b3f711
angeldavila00 <angeldavila0975@gmail.com>
2026-03-09 09:10:56 -0500

    Revert "Five commit"
    This reverts commit 0a76757741e19fa97cf3b47e88ba90853f2e5645.

--- src/main/java/com/example/gestion_tienda/controller/DetalleVentaController.java
+++ src/main/java/com/example/gestion_tienda/controller/DetalleVentaController.java
@@ -1,18 +1,27 @@
 package com.example.gestion_tienda.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.gestion_tienda.dto.request.DetalleVentaRequestDTO;
 import com.example.gestion_tienda.dto.response.DetalleVentaResponseDTO;
 import com.example.gestion_tienda.service.impl.DetalleVentaServiceImpl;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Detalle Venta", description = "Procesa todo lo relacionado con detalle de venta")
 @RestController
@@ -57,7 +66,6 @@ public class DetalleVentaController {
     public ResponseEntity<DetalleVentaResponseDTO> actualizar(@RequestBody DetalleVentaRequestDTO dto, @PathVariable Long id) {
         return ResponseEntity.ok().body(detalleVentaService.actualizarDetalleVenta(dto, id));
     }
-
     @Operation(summary = "Lista los detalles de venta")
     @GetMapping
     public ResponseEntity<List<DetalleVentaResponseDTO>> listarTodos() {
@@ -81,7 +89,6 @@ public class DetalleVentaController {
     public ResponseEntity<DetalleVentaResponseDTO> buscarId(@PathVariable Long id) {
         return ResponseEntity.ok().body(detalleVentaService.buscarPorId(id));
     }
-
     @Operation(summary = "Elimina los detalles de venta")
     @DeleteMapping("/{id}")
     @ApiResponses(value = {
